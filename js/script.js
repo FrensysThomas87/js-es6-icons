5fb8904603d0f213b953524775152db5ff897001
FrensysThomas87 <delta873@gmail.com>
2021-01-21 15:08:21 +0100

    definito l'array di oggetti

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const oggetti = [
+  {
+    'nome': 'scimmia',
+    'prefisso': 'fas',
+    'tipo': 'animali',
+    'famiglia': 'fa'
+  },
+
+  {
+    'nome': 'tigre',
+    'prefisso': 'fas',
+    'tipo': 'animali',
+    'famiglia': 'fa'
+  },
+
+  {
+    'nome': 'cavallo',
+    'prefisso': 'fas',
+    'tipo': 'animali',
+    'famiglia': 'fa'
+  },
+
+  {
+    'nome': 'ferrari',
+    'prefisso': 'fas',
+    'tipo': 'automobili',
+    'famiglia': 'fa'
+  },
+
+  {
+    'nome': 'lamborghini',
+    'prefisso': 'fas',
+    'tipo': 'automobili',
+    'famiglia': 'fa'
+  },
+
+  {
+    'nome': 'computer',
+    'prefisso': 'fas',
+    'tipo': 'tecnologia',
+    'famiglia': 'fa'
+  },
+
+  {
+    'nome': 'smartphone',
+    'prefisso': 'fas',
+    'tipo': 'tecnologia',
+    'famiglia': 'fa'
+  },
+];
